b54266e2dc96a7aa3e086224ffbce6e1f16b2c5c
Vitor <vi7org@hotmail.com>
2024-10-22 13:01:06 -0400

    Switched to process stdout

--- typewriter.js
+++ typewriter.js
@@ -2,8 +2,10 @@ const sentence = "hello there from lighthouse labs";
 let delay = 0;
 
 for (const char of sentence) {
-  setTimeout(()=> {
-    console.log(char);
-  }, delay);
-  delay += 50;
+    setTimeout(()=> {
+        process.stdout.write(char);
+    }, delay)
+    delay += 50;
 }
+
+process.stdout.write("\n");
